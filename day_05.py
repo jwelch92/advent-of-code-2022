be969aefef2fefc72b9b3969b6798348319276dc
Jared Welch <jared@jeli.io>
2022-12-04 21:58:28 -0800

    Cleaned up a bit. Not going to code golf this one

--- day_05.py
+++ day_05.py
@@ -1,3 +1,4 @@
+import string
 from collections import defaultdict
 from pprint import pprint
 from typing import Any
@@ -6,7 +7,6 @@ from lib import run
 
 # AOC DAY 5
 DAY = 5
-col = 3
 # [H]                 [Z]         [J]
 # [L]     [W] [B]     [G]         [R]
 # [R]     [G] [S]     [J] [H]     [Q]
@@ -17,68 +17,59 @@ col = 3
 # [M] [Z] [H] [P] [N] [W] [P] [L] [C]
 #  1   2   3   4   5   6   7   8   9
 
+
 def solve_one(data: str) -> Any:
-    # sub 1 when index
-    m = {i: list() for i in range(1, 10)}
     # -1 is the top of the stack
+    m = {i: list() for i in range(1, 10)}
     raw_crates, moves = data.split("\n\n")
+    # start at the first real column position 1, step by 4 to skip between possible value positions
+    # Enumerate starting at 1 to match our crates 1 based index
     for line in raw_crates.splitlines()[:8]:
-        for count, idx, in enumerate(range(1, len(line), 4), start=1):
+        for (
+            count,
+            idx,
+        ) in enumerate(range(1, len(line), 4), start=1):
             c = line[idx]
             if c and c != " ":
+                # Prepend to form a stack where the last element is the top
                 m[count] = [c] + m[count]
     for line in moves.splitlines():
-        match line.split(" "):
-            case [_, count, _, start, _, end]:
+        count, src, dest = [int(i) for i in line.split(" ") if i.isdigit()]
+        # Extend dest with the top `count` items in reverse order to mimic moving them one by one
+        m[dest].extend(reversed(m[src][-count:]))
+        # trim src by `count` items
+        m[src] = m[src][:-count]
 
-                for i in range(0, int(count)):
-                    m[int(end)].append(m[int(start)].pop())
-
-    tops = []
-    for i in range(1, 10):
-        tops.append(m[i][-1])
-    print("".join(tops))
-    return "".join(tops)
-
+    return "".join([m[i][-1] for i in range(1, 10)])
 
 
 def solve_two(data: str) -> Any:
-    m = {i: list() for i in range(1, 10)}
     # -1 is the top of the stack
+    # Build the crate
+    m = {i: list() for i in range(1, 10)}
     raw_crates, moves = data.split("\n\n")
-    for line in raw_crates.splitlines()[:8]:
-        for count, idx, in enumerate(range(1, len(line), 4), start=1):
+    # Parse starting state
+    for line in raw_crates.splitlines()[
+        :8
+    ]:  # 8 might be a magic number. My input only had stacks up to 8 high
+        for (
+            count,
+            idx,
+        ) in enumerate(range(1, len(line), 4), start=1):
             c = line[idx]
             if c and c != " ":
                 m[count] = [c] + m[count]
+    # process moves
     for line in moves.splitlines():
-        match line.split(" "):
-            case [_, count, _, start, _, end]:
-                count, start, end = int(count), int(start), int(end)
-                print(count, start, end)
-                print("before")
-                print(m[int(start)])
-                print(m[int(end)])
-                print("to move")
-                print(m[int(start)][-1*int(count):])
-
-                m[int(end)].extend(m[int(start)][-1*int(count):])
-                m[start] = m[start][:-1*count:]
-                print("after")
-                print(m[int(start)])
-                print(m[int(end)])
-                # input("halt")
-
-
-
-
-
-
-    tops = []
-    for i in range(1, 10):
-        tops.append(m[i][-1])
-    print("".join(tops))
-    return "".join(tops)
+        # Get the number of crates to move, starting stack, and ending stack
+        # move lines look like this
+        # move 1 from 2 to 1
+        count, src, dest = [int(i) for i in line.split(" ") if i.isdigit()]
+        # Extend dest with the top `count` items
+        m[dest].extend(m[src][-count:])
+        # Trim the top `count` items from src
+        m[src] = m[src][:-count:]
+    return "".join([m[i][-1] for i in range(1, 10)])
 
 
 def main() -> None:
